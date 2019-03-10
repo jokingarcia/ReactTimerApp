bc1e3e267fa3dc73593e67ca6b71b12fb88f238d
Jokin Garcia <jgarcia75@hotmail.com>
2019-03-10 19:59:54 +0100

    Initial commit

---
+++ app/api/example-arrow-functions.js
@@ -0,0 +1,5 @@
+var names = ['Andrew', 'Julie', 'Jen'];
+
+names.forEach(function(name) {
+    console.log('forEach', name);
+});
---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,22 @@
+var axios = require('axios');
+
+var OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=90010aea3087c1dd2362979f96a68b16&units=imperial&q=';
+
+//90010aea3087c1dd2362979f96a68b16
+module.exports = {
+    getTemp: function (location) {
+        var encodedLocation = encodeURIComponent(location);
+        var requestUrl = OPEN_WEATHER_MAP_URL + location;
+        console.log(requestUrl);
+        return axios.get(requestUrl).then(function (res) {
+            console.log(res.data.main.temp);
+            if(res.data.cod && res.data.message){
+                throw new Error(res.data.message);
+            }else{
+                return res.data.main.temp;
+            }
+        }, function (res){
+            throw new Error(res.data.message);
+        });
+    }
+}
